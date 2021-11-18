1d7520f10edcf839ad4135694a1e81ad2fff49da
陈泽江 <820289461@qq.com>
2021-11-18 16:43:48 +0800

    test

---
+++ iframe/avatar/assets/top2.daa9d79d.js
@@ -0,0 +1,10 @@
+var n=`<?xml version="1.0" encoding="UTF-8"?>
+<svg width="40px" height="39px" viewBox="0 0 40 39" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>\u8DEF\u5F84 2</title>
+    <g transform="translate(5, 15)" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="\u753B\u677F" transform="translate(-1455.000000, -265.000000)" fill="#885034">
+            <path d="M1464.26497,303.808271 C1458.17445,295.131004 1455.12919,287.461818 1455.12919,280.800712 C1460.99527,285.994452 1465.40644,288.591323 1468.36272,288.591323 C1464.69606,278.733561 1463.33014,271.13921 1464.26497,265.808271 C1478.20088,280.569969 1488.15562,287.950818 1494.12919,287.950818" id="\u8DEF\u5F84-2"></path>
+        </g>
+    </g>
+</svg>
+`;export{n as default};
