546a6395348b60df9565e527b2ffda3dfb7c546e
陈泽江 <820289461@qq.com>
2023-02-04 21:42:45 +0800

    test

---
+++ static/js/chunk-2d229287.7ba60a93.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d229287"],{dbf9:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},u=[],a="presaleList",c={name:a,data:function(){return{}}},l=c,s=t("5d22"),i=Object(s["a"])(l,r,u,!1,null,null,null);n["default"]=i.exports}}]);
